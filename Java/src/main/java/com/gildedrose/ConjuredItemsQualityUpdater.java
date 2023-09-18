dddf1ba36cf1ba13960a8b1673f3d85aaeb79cad
Stefan <stefan.diels@gmail.com>
2023-09-18 20:16:05 +0200

    fixed conjured items implementation

--- Java/src/main/java/com/gildedrose/ConjuredItemsQualityUpdater.java
+++ Java/src/main/java/com/gildedrose/ConjuredItemsQualityUpdater.java
@@ -1,4 +1,16 @@
 package com.gildedrose;
 
 public class ConjuredItemsQualityUpdater extends BaseQualityUpdater{
+
+    @Override
+    protected void updateBeforeSellIn(Item item) {
+        super.updateBeforeSellIn(item);
+        super.updateBeforeSellIn(item);
+    }
+
+    @Override
+    protected void updateAfterSellIn(Item item) {
+        super.updateAfterSellIn(item);
+        super.updateAfterSellIn(item);
+    }
 }
